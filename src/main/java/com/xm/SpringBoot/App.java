02a31c08ebd3f0aa46d28ea59083723218d0c050
lsp_zone <15712860161@163.com>
2019-07-22 20:54:43 +0800

    千万人

--- src/main/java/com/xm/SpringBoot/App.java
+++ src/main/java/com/xm/SpringBoot/App.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController//11
 @EnableAutoConfiguration
 public class App {
 
